609a22e06f38d79123429c842edb77fdcc9bad97
Gaurav <smartgauravrana@gmail.com>
2021-01-01 15:44:42 +0530

    basic routes added

--- build/index.js
+++ build/index.js
@@ -1,3 +1,19 @@
 "use strict";
-console.log('hello');
-console.log('yes2');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var body_parser_1 = __importDefault(require("body-parser"));
+var cookie_session_1 = __importDefault(require("cookie-session"));
+var http_1 = __importDefault(require("http"));
+var routes_1 = __importDefault(require("./routes"));
+var app = express_1.default();
+var port = process.env.PORT || 3000;
+var server = http_1.default.createServer(app);
+app.use(body_parser_1.default.urlencoded({ extended: true }));
+app.use(cookie_session_1.default({ keys: ['s3crwtyy'] }));
+app.use('/', routes_1.default);
+server.listen(port, function () {
+    console.log("Server running on port " + port);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,22 @@
-console.log('hello');
-console.log('yes2')
+import express from 'express';
+import bodyParser from 'body-parser';
+import cookieSession from 'cookie-session';
+import http from 'http';
+
+import routes from './routes';
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer(app);
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(cookieSession({keys: ['s3crwtyy']}));
+
+app.use('/', routes);
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
