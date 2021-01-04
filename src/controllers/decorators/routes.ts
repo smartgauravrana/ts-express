33abf19aeab0d3bca4a3fc6e5bbbe52e5353d401
Gaurav <smartgauravrana@gmail.com>
2021-01-04 11:58:51 +0530

    added routehandler descriptor type

--- src/controllers/decorators/routes.ts
+++ src/controllers/decorators/routes.ts
@@ -1,9 +1,14 @@
 import 'reflect-metadata';
+import { RequestHandler } from 'express';
 import { MetadataKeys } from './MetadataKeys';
+
+interface RouteHandlerDescriptor extends PropertyDescriptor {
+  value?: RequestHandler;
+}
 
 function routeBinder(method: string) {
   return function(path: string) {
-    return function(target: any, key: string, desc: PropertyDescriptor) {
+    return function(target: any, key: string, desc: RouteHandlerDescriptor) {
       Reflect.defineMetadata(MetadataKeys.path, path, target, key);
       Reflect.defineMetadata(MetadataKeys.method, method, target, key);
     }
